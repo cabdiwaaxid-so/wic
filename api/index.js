22bdd08b119fe3bf12a2e60c3dd39f9b0e9231db
Cabdi Waaxid Siciid <158821136+cabdiwaaxid-so@users.noreply.github.com>
2026-01-09 06:30:21 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,1 +1,28 @@
+const tf = require('@tensorflow/tfjs-node');
+const nsfw = require('nsfwjs');
+const sharp = require('sharp');
+const fs = require('fs');
+const novax = require('novaxjs2');
+const app = new novax();
+const model = await nsfw.load();
 
+async function checkImage(path) {
+  const imageBuffer = await sharp(path)
+    .resize(256, 256)
+    .toBuffer();
+
+  const image = tf.node.decodeImage(imageBuffer, 3);
+  const predictions = await model.classify(image);
+  image.dispose();
+
+  return predictions;
+}
+
+app.post('/', async(req, res) => {
+  try {
+    const { imagePath } = req.body;
+    res.json(await checkImage(imagePath))
+  } catch(err) {
+    res.json(err)
+  }
+})
